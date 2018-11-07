58522268b9cd4d4c66fb5bd22579bb46421cc474
Burton428 <ADAMBURTON428@users.noreply.github.com>
2018-11-07 13:00:48 -0600

    update

---
+++ MilesToKiloMeterGUI/MilesToKiloMeterGUI/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace MilesToKiloMeterGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.lblKilometers = new System.Windows.Forms.Label();
+            this.txtMiles = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(285, 89);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 0;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // lblKilometers
+            // 
+            this.lblKilometers.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblKilometers.Location = new System.Drawing.Point(389, 89);
+            this.lblKilometers.Name = "lblKilometers";
+            this.lblKilometers.Size = new System.Drawing.Size(100, 23);
+            this.lblKilometers.TabIndex = 1;
+            // 
+            // txtMiles
+            // 
+            this.txtMiles.Location = new System.Drawing.Point(153, 89);
+            this.txtMiles.Name = "txtMiles";
+            this.txtMiles.Size = new System.Drawing.Size(100, 20);
+            this.txtMiles.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtMiles);
+            this.Controls.Add(this.lblKilometers);
+            this.Controls.Add(this.btnConvert);
+            this.Name = "Form1";
+            this.Text = "MilesToKilometers";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Label lblKilometers;
+        private System.Windows.Forms.TextBox txtMiles;
+    }
+}
+
