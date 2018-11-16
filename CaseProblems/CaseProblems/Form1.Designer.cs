9cd9e418692f0989059a591485f63ada404858bb
Burton428 <ADAMBURTON428@users.noreply.github.com>
2018-11-16 12:59:53 -0600

    update

--- CaseProblems/CaseProblems/Form1.Designer.cs
+++ CaseProblems/CaseProblems/Form1.Designer.cs
@@ -43,9 +43,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(101, 61);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(52, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Last Year";
             // 
             // textBox1
             // 
@@ -66,9 +66,9 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(261, 61);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(52, 13);
             this.label2.TabIndex = 3;
-            this.label2.Text = "label2";
+            this.label2.Text = "This Year";
             // 
             // button1
             // 
@@ -76,7 +76,6 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 4;
-            this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // label3
